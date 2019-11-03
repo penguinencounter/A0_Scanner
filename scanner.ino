5f17ea20a1ec91faa97e25f792cd6a0b00029331
Miles <miles.h.lin@gmail.com>
2019-11-03 10:08:47 -0800

    Add the light

--- scanner.ino
+++ scanner.ino
@@ -1,10 +1,15 @@
+#define BRIGHTNESS = 128
+
 void setup() {
     Serial.begin(9600);
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB port only
     }
 }
+
+// Use pin 3 for PWM lighting
 void loop() {
+    analogWrite(3, BRIGHTNESS);
     int reading = analogRead(A0);
     Serial.print(reading);
     Serial.print('\n');
