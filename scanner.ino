96657ae9e8ada57688051a0bf4662f75ef7aa742
Miles <miles.h.lin@gmail.com>
2019-11-11 15:36:11 -0800

    Yeet!

--- scanner.ino
+++ scanner.ino
@@ -1,6 +1,7 @@
-#define BRIGHTNESS 128
 
 void setup() {
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
     Serial.begin(9600);
     while (!Serial) {
         ; // wait for serial port to connect. Needed for native USB port only
@@ -9,7 +10,7 @@ void setup() {
 
 // Use pin 3 for PWM lighting
 void loop() {
-    analogWrite(3, BRIGHTNESS);
+    
     int reading = analogRead(A0);
     Serial.print(reading);
     Serial.print('\n');
