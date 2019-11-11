90032154e484713d01ecb4731f77bf5027af0fc0
Miles <miles.h.lin@gmail.com>
2019-11-11 15:51:32 -0800

    Fixed. LED represents Serial Prot connection.

--- scanner.ino
+++ scanner.ino
@@ -2,10 +2,19 @@
 void setup() {
     pinMode(2, OUTPUT);
     digitalWrite(2, HIGH);
-    Serial.begin(9600);
-    while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB port only
+    pinMode(13, OUTPUT);
+    if (Serial) {
+        digitalWrite(13, HIGH);
+        Serial.begin(9600);
+        while (!Serial) {
+            ; // wait for serial port to connect. Needed for native USB port only
+        }
+        Serial.print('Serial port connected.')
+    } else {
+        digitalWrite(13, LOW);
     }
+    delay(1000);
+    digitalWrite(13, LOW);
 }
 
 // Use pin 3 for PWM lighting
